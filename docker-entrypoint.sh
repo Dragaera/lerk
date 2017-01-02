ecaeed1f3a972a14854b7ad40d6192e08060a7c7
Michael Senn <michael@senn.email>
2017-01-02 18:54:28 +0100

    Adds docker entrypoint.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case "$1" in
+    bot)
+        echo "Starting discord bot..."
+        exec ruby -Ilib bin/lerk
+        ;;
+    *)
+        echo "Don't know what to do with $1"
+        echo "Valid commands: bot"
+        exit 1
+esac
