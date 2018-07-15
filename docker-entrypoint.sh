b5c8f620909a13c82904199de5f6e6caa99aeeac
Michael Senn <michael@senn.email>
2018-07-15 21:54:22 +0200

    Add Docker entrypoint to apply migrations.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,11 @@ case "$1" in
         echo "Starting discord bot..."
         exec ruby bin/lerk
         ;;
+    migrate)
+        echo "Applying database migrations"
+        exec rake db:migrate
     *)
         echo "Don't know what to do with $1"
-        echo "Valid commands: bot"
+        echo "Valid commands: bot, migrate"
         exit 1
 esac
