6e5fa25acffbdeb16b9ce276f2b8f202caa46ad9
Michael Senn <michael@morrolan.ch>
2018-08-23 20:44:02 +0200

    Export read tips to database.

--- bin/import_tips_sheet.rb
+++ bin/import_tips_sheet.rb
@@ -5,7 +5,8 @@ require 'config/boot'
 source = Lerk::Hints::HTTPDownloadSource.new
 # source = Lerk::Hints::LocalFileSource.new('/var/tmp/ns2_tips_sheet.csv')
 parser = Lerk::Hints::Parser.new(source: source)
-
 hints = parser.parse
+exporter = Lerk::Hints::SequelExporter.new(hints)
+exporter.export
 
 puts "Got #{ hints.length } hints."
