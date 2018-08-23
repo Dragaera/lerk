19655a87a339907777d0d4c39fa7ebdc397e67a8
Michael Senn <michael@morrolan.ch>
2018-08-23 20:42:58 +0200

    Add `!tiplist` command.

--- lib/lerk/hints.rb
+++ lib/lerk/hints.rb
@@ -50,6 +50,16 @@ module Lerk
           command_hint(event, tag: arg1)
         end
       end
+
+      @bot.command(
+        [:tiplist],
+        description: 'Various sources of tips/hints',
+        usage: '!tiplist [category]',
+        min_args: 0,
+        max_args: 1,
+      ) do |event, arg|
+        command_tiplist(event, arg)
+      end
     end
 
     def self.command_hint(event, group: nil, tag: nil)
@@ -71,6 +81,20 @@ module Lerk
 
     rescue ArgumentError => e
       e.message
+    end
+
+    def self.command_tiplist(event, arg)
+      if arg.nil?
+        'Check Sticky Messages in #newbie-corner in the official NS2 Discord!'
+      elsif arg == 'guide'
+        "List of Advanced Tutorials:\nhttps://steamcommunity.com/sharedfiles/filedetails/?id=1359882555"
+      elsif arg == 'movement'
+        "This is the Movement Mechanics Tutorial that explains the Movement of NS2.\nhttps://steamcommunity.com/sharedfiles/filedetails/?id=1416909558\nIt included a keyboard overlay that should help you to learn how to it works :smiley:"
+      elsif arg == 'voyeur'
+        'https://www.twitch.tv/directory/game/Natural%20Selection%20II/videos/all'
+      else
+        "Invalid argument. Pick one of: (guide, movement, voyeur)"
+      end
     end
   end
 end
