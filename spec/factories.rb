f72cae2f00530e081fc497d717b589d966d19239
Michael Senn <michael@morrolan.ch>
2020-11-01 16:56:57 +0100

    Update bundler and dependencies.

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,20 +4,20 @@ module Lerk
 
     factory :discord_user, class: 'Lerk::DiscordUser' do
       sequence(:discord_id)
-      last_nick 'John'
+      last_nick { 'John' }
     end
 
     factory :event, class: 'Lerk::Event' do
       sequence(:key)
-      stats_output_description ''
-      stats_output_order 1
-      show_in_stats_output false
+      stats_output_description { '' }
+      stats_output_order { 1 }
+      show_in_stats_output { false }
     end
 
     factory :event_counter, class: 'Lerk::EventCounter' do
       event
       discord_user
-      count 0
+      count { 0 }
     end
 
     factory :hint_tag, class: 'Lerk::HintTag' do
